c4a7929a8043786ab53ae526a3e3cea8b6ec53b4
wenjayliu <865015063@qq.com>
2018-08-25 22:51:18 +0800

    简单国际化

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 飞冰初体验
 ## 初始化项目
-## 多环境变量和模式 。
+### 多环境变量和模式 。
 我需要打包三分不同的版本，又不想运行 `eject` 那不可逆的操作，再加上貌似 react 构建的时候 `NODE_ENV` 还无法修改。
 所以我的解决方法是新建一个环境变量，通过这个环境变量再来设置全局常量。
 
@@ -38,4 +38,8 @@ module.exports = {
 }
 
 ```
+
+### 国际化支持
+
+采用阿里的 [react-intl-universal](https://github.com/alibaba/react-intl-universal)
 
--- src/App.js
+++ src/App.js
@@ -2,14 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { REACT_APP_ENV } from './config'
+import intl from 'react-intl-universal';
+
+// locale data
+const locales = {
+  "en-US": require('./locales/en-US.json'),
+  "zh-CN": require('./locales/zh-CN.json')
+}
 console.log('环境', REACT_APP_ENV)
+console.log('语言', locales)
+
 class App extends Component {
+  state = {initDone: false}
+
+  componentDidMount() {
+    this.loadLocales();
+  }
+
+  loadLocales() {
+    // init method will load CLDR locale data according to currentLocale
+    // react-intl-universal is singleton, so you should init it only once in your app
+    intl.init({
+      currentLocale: 'en-US', // TODO: determine locale here
+      locales,
+    })
+    .then(() => {
+      // After loading CLDR locale data, start to render
+	  this.setState({initDone: true});
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          {intl.get('TIP')}
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
